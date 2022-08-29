811cb0c55fd07ca5e1575d577f8cf6e2a9edcef5
Stephen Golban <golban.stephen@gmail.com>
2022-08-29 06:41:05 +0300

    trying to deploy

--- pages/api/categories.ts
+++ pages/api/categories.ts
@@ -5,5 +5,5 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function getCategories(req: NextApiRequest, res: NextApiResponse) {
   const data = db.data;
 
-  return res.status(200).json(data);
+  return res.status(200).json(JSON.parse(JSON.stringify(data)));
 }
