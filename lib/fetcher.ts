18edd6fe5937d418aef1f082ef0337d49e0c1bdf
Stephen Golban <golban.stephen@gmail.com>
2022-08-29 06:45:00 +0300

    trying to deploy

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,5 +1,11 @@
 export const fetcher = async (url: string) => {
-  const res = await fetch(url);
+  const res = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'User-Agent': '*',
+    },
+  });
 
   return await res.json();
 };
